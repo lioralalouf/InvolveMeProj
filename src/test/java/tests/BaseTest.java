47860e3938a9da1eca6db952602caf9a2c87e68c
lioralalouf <alalouflior@gmail.com>
2021-06-21 12:55:21 +0300

    web driver manager chrom

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -8,12 +8,13 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestContext;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 import pageObjects.HomePage;
 import pageObjects.LoginPage;
 import utils.Utils;
@@ -22,11 +23,12 @@ public class BaseTest {
 	WebDriver driver;
 
 	@BeforeClass
-	public void setup() {
-		System.setProperty("webdriver.chrome.driver", "C:\\automation\\drivers\\chromedriver.exe");
+	public void setup(ITestContext testContext) {
+		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get(Utils.readProperty("url"));
+		testContext.setAttribute("WebDriver", this.driver);
 	}
 	@BeforeClass
 	public void validLogin() {
